b91dbb35573de57a6291d0171600a79f950eb1a1
Anton Hallstrom <antonhallstrom@live.se>
2019-04-22 20:43:37 +0200

    fo

--- webpack.common.js
+++ webpack.common.js
@@ -14,7 +14,7 @@ module.exports = {
       ARA_API_URL: JSON.stringify(process.env.ARA_API_URL),
     }),
     new DotenvPlugin({
-      path: './.env',
+      path: '.env',
     }),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
