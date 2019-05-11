b92a5654b77f868ec347d948cbbe3093781b601a
Anton Hallstrom <antonhallstrom@live.se>
2019-05-11 14:56:45 +0200

    loader for images

---
+++ src/components/composites/image.js
@@ -0,0 +1,90 @@
+import PropTypes from 'prop-types'
+import * as R from 'ramda'
+import React, { useState, useEffect, useRef } from 'react'
+import styled from '@emotion/styled'
+
+const Placeholder = styled.div`
+  background-color: #f6f6f6;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: relative;
+  overflow: hidden;
+  height: ${props => (props.height ? `${props.height}px` : `100%`)};
+  width: ${props => props.width}px;
+  min-height: 296px;
+`
+
+const AspectRatioFill = styled.div`
+  padding-bottom: 66.6%;
+`
+
+const Img = styled.img`
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  transition: opacity 1s linear;
+  object-fit: cover;
+  filter: blur(50px);
+  /* this is needed so Safari keeps sharp edges */
+  transform: scale(1);
+
+  ${props =>
+    props.loaded &&
+    `
+    opacity: 1;
+  `}
+`
+
+const ReadyImage = styled.img`
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  transition: opacity 1s linear;
+  object-fit: cover;
+  ${props =>
+    props.loaded &&
+    `
+    opacity: 1;
+  `}
+`
+
+export function Image(props) {
+  const [loaded, setProcessing] = useState({})
+
+  const small = useRef()
+  const large = useRef()
+
+  useEffect(
+    (window.onload = () => {
+      setProcessing({
+        small: R.pathOr(false, ['complete'], small.current),
+        large: R.pathOr(false, ['complete'], large.current),
+      })
+    }),
+    [R.path(['complete'], small.current), R.path(['complete'], large.current)]
+  )
+
+  return (
+    <div>
+      <Placeholder width={props.width} height={props.height}>
+        <div loaded={loaded.small} ref={small} src={props.small} />
+        <Img loaded={loaded.small} ref={small} src={props.small} />
+        <ReadyImage ref={large} loaded={loaded.large} src={props.large} />
+        <AspectRatioFill />
+      </Placeholder>
+    </div>
+  )
+}
+
+Image.propTypes = {
+  height: PropTypes.string,
+  width: PropTypes.string,
+  large: PropTypes.string.isRequired,
+  small: PropTypes.string.isRequired,
+}
--- src/views/artwork/artwork.js
+++ src/views/artwork/artwork.js
@@ -1,26 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { Constraint, Flex, Space } from '../../components/elements'
 import * as Layout from '../../components/layouts'
-import styled from '@emotion/styled'
+import * as composites from '../../components/composites'
 
 // Make responsive images, so they take full width on small device,
 // also center content
 // make image previwer when hover or clicking the images.
-
-const Image = styled.img`
-  height: ${props => (props.height ? `${props.height}px` : `100%`)};
-  width: ${props => props.width}px;
-`
-
-const BackgroundImage = styled.div`
-  background-image: url(${props => props.filename});
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  width: 288px;
-  height: 100%;
-  min-height: 288px;
-`
 
 export function Artwork() {
   const [layout, setLayout] = useState(null)
@@ -44,6 +29,7 @@ export function Artwork() {
     }
   }, [])
 
+  // TODO: React error, can't do state update on an unmounted component.
   useEffect(
     () => {
       window.addEventListener('resize', () => {
@@ -66,20 +52,41 @@ export function Artwork() {
           {layout === 'large' && (
             <Flex style={{ height: '600px' }}>
               <Space right="1">
-                <Image src="face.jpg" width="600" />
+                <composites.Image
+                  small="small-face.png"
+                  large="face.jpg"
+                  width="600"
+                  height="600"
+                />
               </Space>
               <Flex wrap="true">
                 <Space bottom="0" right="0">
-                  <BackgroundImage filename="background.jpg" />
+                  <composites.Image
+                    small="small-background.png"
+                    large="background.jpg"
+                    width="290"
+                  />
                 </Space>
                 <Space left="0" bottom="0">
-                  <BackgroundImage filename="marsland.jpg" />
+                  <composites.Image
+                    small="small-marsland.png"
+                    large="marsland.jpg"
+                    width="290"
+                  />
                 </Space>
                 <Space top="0" right="0">
-                  <BackgroundImage filename="figure.jpg" />
+                  <composites.Image
+                    small="small-figure.png"
+                    large="figure.jpg"
+                    width="290"
+                  />
                 </Space>
                 <Space left="0" top="0">
-                  <BackgroundImage filename="roboface.jpg" />
+                  <composites.Image
+                    small="small-roboface.png"
+                    large="roboface.jpg"
+                    width="290"
+                  />
                 </Space>
               </Flex>
             </Flex>
@@ -88,19 +95,44 @@ export function Artwork() {
             <Flex justify="center" basis="400px">
               <Flex wrap="true">
                 <Space all="0">
-                  <BackgroundImage filename="face.jpg" />
+                  <composites.Image
+                    small="small-face.png"
+                    large="face.jpg"
+                    width="288"
+                    height="288"
+                  />
                 </Space>
                 <Space all="0">
-                  <BackgroundImage filename="background.jpg" />
+                  <composites.Image
+                    small="small-background.png"
+                    large="background.jpg"
+                    width="288"
+                    height="288"
+                  />
                 </Space>
                 <Space all="0">
-                  <BackgroundImage filename="marsland.jpg" />
+                  <composites.Image
+                    small="small-marsland.png"
+                    large="marsland.jpg"
+                    width="288"
+                    height="288"
+                  />
                 </Space>
                 <Space all="0">
-                  <BackgroundImage filename="figure.jpg" />
+                  <composites.Image
+                    small="small-figure.png"
+                    large="figure.jpg"
+                    width="288"
+                    height="288"
+                  />
                 </Space>
                 <Space all="0">
-                  <BackgroundImage filename="roboface.jpg" />
+                  <composites.Image
+                    small="small-roboface.png"
+                    large="roboface.jpg"
+                    width="288"
+                    height="288"
+                  />
                 </Space>
               </Flex>
             </Flex>
